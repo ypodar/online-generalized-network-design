62ae692ec674f24d35d9c763b2e23aa5b265b093
ypodar <ypodar@umich.edu>
2022-06-03 21:58:30 -0400

    changes to abilene and nsf files and final edits

--- main.py
+++ main.py
@@ -232,6 +232,132 @@ def algo_main(network, pairs, alpha, qval, sigma):
             algo_sum += sigma[j]
     return algo_sum
 
+rerun_list = [
+    "runs/abilene_c_k5_1.txt",
+    "runs/abilene_c_k5_2.txt",
+    "runs/abilene_c_k10_1.txt",
+    "runs/abilene_c_k10_2.txt",
+    "runs/abilene_c_k20_1.txt",
+    "runs/abilene_c_k20_2.txt",
+    "runs/abilene_d_k5_1.txt",
+    "runs/abilene_d_k5_2.txt",
+    "runs/abilene_d_k10_1.txt",
+    "runs/abilene_d_k10_2.txt",
+    "runs/abilene_d_k20_1.txt",
+    "runs/abilene_d_k20_2.txt",
+    "runs/nsf_c_k5_1.txt",
+    "runs/nsf_c_k5_2.txt",
+    "runs/nsf_c_k10_1.txt",
+    "runs/nsf_c_k10_2.txt",
+    "runs/nsf_c_k20_1.txt",
+    "runs/nsf_c_k20_2.txt",
+    "runs/nsf_d_k5_1.txt",
+    "runs/nsf_d_k5_2.txt",
+    "runs/nsf_d_k10_1.txt",
+    "runs/nsf_d_k10_2.txt",
+    "runs/nsf_d_k20_1.txt",
+    "runs/nsf_d_k20_2.txt"
+]
+
+
+def algorithm_run(network, outputpairs, alpha, qval, sigma):
+    print("----------Algorithm----------")
+    for x in range(10):
+        random.shuffle(outputpairs)
+        print(algo_main(network, outputpairs, alpha, qval, sigma))
+
+
+
+def IPrun(network, outputpairs, alpha, qval, sigma):
+    print("----------Integer Program----------")
+    undirected_network = []
+    for i in range(len(network)):
+        a = network[i]
+        if network[i] not in undirected_network and (a[1], a[0]) not in undirected_network:
+            undirected_network.append(network[i])
+    lin_network = gb.tuplelist(network)
+    lin_undirected_network = gb.tuplelist(undirected_network)
+    linModel = gb.Model("Integer Program")
+    flow = linModel.addVars(lin_network, vtype=GRB.CONTINUOUS, name="Flow", lb=0)
+    xval = {}
+    z = {}
+    k = len(outputpairs)
+    tuple_i = gb.tuplelist(list(range(0, k + 1)))  # [0:k]
+    for i in outputpairs:
+        xval[i] = linModel.addVars(lin_network, vtype=GRB.BINARY)
+    for j in undirected_network:
+        z[j] = linModel.addVars(tuple_i, vtype=GRB.BINARY)
+        reverse_arc = (j[1], j[0])
+        linModel.addConstr(flow[j] == gb.quicksum(xval[pair][j] + xval[pair][reverse_arc] for pair in outputpairs))
+    for path in xval:
+        for node in nodes:
+            if node == path[0]:
+                val = -1
+            elif node == path[1]:
+                val = 1
+            else:
+                val = 0
+            linModel.addConstr(gb.quicksum(xval[path][edge] for edge in lin_network.select('*', node, '*')) -
+                               gb.quicksum(xval[path][edge] for edge in lin_network.select(node, '*', '*')) == val)
+
+    for edge in undirected_network:
+        reverse_arc = (edge[1], edge[0])
+        linModel.addConstr(gb.quicksum(z[edge][i] for i in tuple_i) == 1)
+        for i in range(0, k + 1):
+            # linModel.addConstr(k * z[edge][i] >= flow[edge] - i + 1)
+            linModel.addConstr(k * (1 - z[edge][i]) >= i - (flow[edge] + flow[reverse_arc]))
+            linModel.addConstr(k * (1 - z[edge][i]) >= (flow[edge] + flow[reverse_arc]) - i)
+
+    # calculate flow of edge i, j and j, i (but don't double count - only count for 1 edge)
+    # could use only when i < j
+    linModel.setObjective(gb.quicksum(gb.quicksum(((z[edge][i] * (i ** alpha[(edge[0], edge[1])])) for i in tuple_i)) +
+                                      (sigma[(edge[0], edge[1])] * (1 - z[edge][0])) for
+                                      edge in lin_undirected_network), GRB.MINIMIZE)
+    linModel.setParam("OutputFlag", 0)  # turn off output reporting
+    # time limit is only for computation, model building time is not included
+    linModel.setParam('TimeLimit', 500)
+    linModel.optimize()
+
+    print("Objective Value:")
+    print(linModel.objVal)
+    print("Best Lower Bound:")
+    print(linModel.ObjBound)
+    print("Runtime:")
+    print(linModel.Runtime)
+
+rerun_list = [
+    "runs/abilene_c_k5_1.txt",
+    "runs/abilene_c_k5_2.txt",
+    "runs/abilene_c_k10_1.txt",
+    "runs/abilene_c_k10_2.txt",
+    "runs/abilene_c_k20_1.txt",
+    "runs/abilene_c_k20_2.txt",
+    "runs/abilene_d_k5_1.txt",
+    "runs/abilene_d_k5_2.txt",
+    "runs/abilene_d_k10_1.txt",
+    "runs/abilene_d_k10_2.txt",
+    "runs/abilene_d_k20_1.txt",
+    "runs/abilene_d_k20_2.txt",
+    "runs/nsf_c_k5_1.txt",
+    "runs/nsf_c_k5_2.txt",
+    "runs/nsf_c_k10_1.txt",
+    "runs/nsf_c_k10_2.txt",
+    "runs/nsf_c_k20_1.txt",
+    "runs/nsf_c_k20_2.txt",
+    "runs/nsf_d_k5_1.txt",
+    "runs/nsf_d_k5_2.txt",
+    "runs/nsf_d_k10_1.txt",
+    "runs/nsf_d_k10_2.txt",
+    "runs/nsf_d_k20_1.txt",
+    "runs/nsf_d_k20_2.txt"
+]
+#
+# for file in rerun_list:
+#     print(file)
+#     network, outputpairs, nodes, alpha, qval, sigma = fileRead(file)
+#     algorithm_run(network, outputpairs, alpha, qval, sigma)
+#     IPrun(network, outputpairs, alpha, qval, sigma)
+#     print("--------------------------------")
 
 abilene = [(1, 0), (1, 3), (0, 3), (0, 2), (2, 5), (3, 4), (4, 5), (4, 6), (5, 7), (6, 8), (7, 9), (8, 9)]  # 10 nodes
 nsf = [(0, 1), (0, 8), (0, 2), (1, 3), (1, 2), (2, 5), (3, 4), (3, 10), (4, 5), (4, 6), (5, 7), (5, 12), (6, 8), (7, 9),
@@ -241,68 +367,10 @@ nsf = [(0, 1), (0, 8), (0, 2), (1, 3), (1, 2), (2, 5), (3, 4), (3, 10), (4, 5), 
 # to run reader - change between nsf and abilene for first 2 parameters. c/d for 3rd, # of pairs to generate for 4th,
 # and instance # for 5th
 numPairs = 40  # run between 5, 10, and 20
-temp_nodes, temp_edges = generateEdges(100)
-network, outputpairs, nodes, alpha, qval, sigma = fileRead(fileGen(temp_edges, temp_nodes, "random", "d", numPairs, 2))
+nodes, edges = generateEdges(100)
+
+# fileGen inputs: list of edges and nodes, networkname, c/d (cont or discrete), number of pairs, and instance #
+# network, outputpairs, nodes, alpha, qval, sigma = fileRead(fileGen(edges, nodes, "random", "d", numPairs, 2))
 # network, outputpairs, nodes, alpha, qval, sigma = fileRead("random_n50_c_k20_1.txt")
-
-print("----------Algorithm----------")
-print("Pairs:", numPairs)
-for x in range(10):
-    random.shuffle(outputpairs)
-    print(algo_main(network, outputpairs, alpha, qval, sigma))
-
-print("----------Integer Program----------")
-undirected_network = []
-for i in range(len(network)):
-    a = network[i]
-    if network[i] not in undirected_network and (a[1], a[0]) not in undirected_network:
-        undirected_network.append(network[i])
-lin_network = gb.tuplelist(network)
-lin_undirected_network = gb.tuplelist(undirected_network)
-linModel = gb.Model("Integer Program")
-flow = linModel.addVars(lin_network, vtype=GRB.CONTINUOUS, name="Flow", lb=0)
-xval = {}
-z = {}
-k = len(outputpairs)
-tuple_i = gb.tuplelist(list(range(0, k + 1)))  # [0:k]
-for i in outputpairs:
-    xval[i] = linModel.addVars(lin_network, vtype=GRB.BINARY)
-for j in undirected_network:
-    z[j] = linModel.addVars(tuple_i, vtype=GRB.BINARY)
-    reverse_arc = (j[1], j[0])
-    linModel.addConstr(flow[j] == gb.quicksum(xval[pair][j] + xval[pair][reverse_arc] for pair in outputpairs))
-for path in xval:
-    for node in nodes:
-        if node == path[0]:
-            val = -1
-        elif node == path[1]:
-            val = 1
-        else:
-            val = 0
-        linModel.addConstr(gb.quicksum(xval[path][edge] for edge in lin_network.select('*', node, '*')) -
-                           gb.quicksum(xval[path][edge] for edge in lin_network.select(node, '*', '*')) == val)
-
-for edge in undirected_network:
-    reverse_arc = (edge[1], edge[0])
-    linModel.addConstr(gb.quicksum(z[edge][i] for i in tuple_i) == 1)
-    for i in range(0, k + 1):
-        # linModel.addConstr(k * z[edge][i] >= flow[edge] - i + 1)
-        linModel.addConstr(k * (1 - z[edge][i]) >= i - (flow[edge] + flow[reverse_arc]))
-        linModel.addConstr(k * (1 - z[edge][i]) >= (flow[edge] + flow[reverse_arc]) - i)
-
-# calculate flow of edge i, j and j, i (but don't double count - only count for 1 edge)
-# could use only when i < j
-linModel.setObjective(gb.quicksum(gb.quicksum(((z[edge][i] * (i ** alpha[(edge[0], edge[1])])) for i in tuple_i)) +
-                                  (sigma[(edge[0], edge[1])] * (1 - z[edge][0])) for
-                                  edge in lin_undirected_network), GRB.MINIMIZE)
-# linModel.setParam("OutputFlag", 0)  # turn off output reporting
-# time limit is only for computation, model building time is not included
-linModel.setParam('TimeLimit', 500)
-linModel.optimize()
-
-print("Objective Value:")
-print(linModel.objVal)
-print("Best Lower Bound:")
-print(linModel.ObjBound)
-print("Runtime:")
-print(linModel.Runtime)
+# algorithm_run(network, outputpairs, nodes, alpha, qval, sigma)
+# IPrun(network, outputpairs, nodes, alpha, qval, sigma)
